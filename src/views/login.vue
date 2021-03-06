befed10a62cb37ffba45e74f944a0ba38a7358f6
jijianchang <2235516049@qq.com>
2021-03-06 12:11:32 +0800

    登录功能

---
+++ src/views/login.vue
@@ -0,0 +1,64 @@
+<template>
+<div class="login">
+  <el-form style="width: 20%" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+    <el-form-item label="账号" prop="username">
+      <el-input v-model="ruleForm.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="ruleForm.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+      <el-button @click="resetForm('ruleForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
+
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      const _this=this
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          axios.post('http://localhost:8081/user/login',_this.ruleForm).then(function (resp){
+            console.log(resp)
+        })
+        }else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
